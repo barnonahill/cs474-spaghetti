bf0ebb0f8ab6e37c57fe69cae223856ed486a107
Barnonahill <ptidal@gmail.com>
2018-04-15 21:08:53 -0400

    Added abstract servlet

---
+++ tomcat/src/spg/servlets/SpgHttpServlet.java
@@ -0,0 +1,46 @@
+package spg.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Abstract implementation of an HTTP Servlet. We don't care if Requests are GETs or POSTs.
+ *
+ */
+public abstract class SpgHttpServlet extends HttpServlet {
+	
+	public final void doGet(HttpServletRequest req, HttpServletResponse res) {
+		handleRequest(req, res);
+	}
+	
+	public final void doPost(HttpServletRequest req, HttpServletResponse res) {
+		handleRequest(req, res);
+	}
+	
+	public abstract void handleRequest(HttpServletRequest req, HttpServletResponse res);
+	
+	/**
+	 * Writes a JSON String to response.
+	 * @param res
+	 * @param msg
+	 * @throws IOException could not get res writer
+	 */
+	public void wsriteResponse(HttpServletResponse res, String msg) throws IOException {
+		PrintWriter out = null;
+		try {
+			res.setContentType("application/json");
+			res.setHeader("Cache-Control", "no-cache");
+			out = res.getWriter();
+			out.write(msg);
+		}
+		finally {
+			if (out != null) {
+				out.close();
+			}
+		}
+	}
+}
