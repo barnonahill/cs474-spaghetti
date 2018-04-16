094313730213e034cc694a846de9a8afbb35b918
Carl Clermont <theCarlClermont@gmail.com>
2018-04-15 22:42:16 -0400

    Spg controller.
    connects to the db, executes a sql command given to it.

---
+++ tomcat/src/spg/controllers/SpgController.java
@@ -0,0 +1,35 @@
+package spg.controllers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.http.HttpServlet;
+
+public abstract class SpgController extends HttpServlet{
+	
+	public static final String database = "jdbc:mysql://mysql.cs.jmu.edu";
+	public static final String user = "clermocj";
+	public static final String dbPass = "cs474";
+	
+	/**
+	 * makes a connection to the database and returns whatever the query that was received outputs.
+	 * @param queryString - the sql command to execute.
+	 * @return - the resultSet of the sql command.
+	 * @throws Exception - if it cant connect to the server.
+	 */
+	public final ResultSet getResultSet(String queryString) throws Exception {
+		Connection connection;
+		Statement statement;
+		try {
+			connection = DriverManager.getConnection(database, user, dbPass);
+			statement = connection.createStatement();
+			return statement.executeQuery(queryString);
+		} catch (Exception e) {
+	        e.printStackTrace();
+	        throw new Exception("Could not connect to database");
+		}
+	}
+	
+}
