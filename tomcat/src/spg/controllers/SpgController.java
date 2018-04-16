d634a46b7924a0f6b249188e3f34b959b56be2d4
Clermont <clermocj@l24807.cs.jmu.edu>
2018-04-16 11:28:17 -0400

    spg Contrller updated to actually use a database

--- tomcat/src/spg/controllers/SpgController.java
+++ tomcat/src/spg/controllers/SpgController.java
@@ -9,14 +9,15 @@ import javax.servlet.http.HttpServlet;
 
 /**
  * @author Carl Clermont
- * @version 4/15/18
+ * @version 4/16/18
  */
 
 public abstract class SpgController extends HttpServlet{
 	
-	public static final String database = "jdbc:mysql://mysql.cs.jmu.edu";
-	public static final String user = "clermocj";
-	public static final String dbPass = "cs474";
+	public static final String DATABASE_ADDR = "jdbc:mysql://mysql.cs.jmu.edu";
+	public static final String USER = "clermocj";
+	public static final String DB_PASS = "cs474";
+	public static final String DATABASE_ADDR = "BarnhillButtsClermontTran_Manuscript";
 	
 	/**
 	 * makes a connection to the database and returns whatever the query that was received outputs.
@@ -28,8 +29,9 @@ public abstract class SpgController extends HttpServlet{
 		Connection connection;
 		Statement statement;
 		try {
-			connection = DriverManager.getConnection(database, user, dbPass);
+			connection = DriverManager.getConnection(DATABASE_ADDR, USER, DB_PASS);
 			statement = connection.createStatement();
+			stmt.executeQuery("USE "+ DATABASE_NAME);
 			return statement.executeQuery(queryString);
 		} catch (Exception e) {
 	        e.printStackTrace();
