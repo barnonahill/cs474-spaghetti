5d3eabd9a1bd227d464aecb4ca2a6b6e41e3f979
Barnonahill <ptidal@gmail.com>
2018-04-16 22:20:27 -0400

    LibraryProxy refactoring

--- node/src/proxies/LibraryProxy.ts
+++ node/src/proxies/LibraryProxy.ts
@@ -1,3 +1,8 @@
+import * as cty from '@src/models/century';
+import * as crs from '@src/models/cursus';
+import * as sc from '@src/models/sourceCompleteness';
+import * as prv from '@src/models/provenance';
+import * as ntn from '@src/models/notation';
 import * as lib from '@src/models/Library';
 import SpgProxy from '@src/proxies/SpgProxy';
 
@@ -10,6 +15,111 @@ import SpgProxy from '@src/proxies/SpgProxy';
 export default class LibraryProxy extends SpgProxy {
 	constructor() {
 		super('library');
+	}
+
+	createCentury(props: cty.Properties, callback: (c: cty.Century, err?: string) => void) {
+		var params = {
+			action: 'CreateCentury',
+			century: props
+		};
+
+		super.doPost(params, (res: any) => {
+			if (res.err) {
+				callback(null, res.err);
+			}
+			else {
+				callback(new cty.Century(res as cty.Properties), null);
+			}
+		});
+	}
+
+	getCentury(centuryID: string, callback: (century: cty.Century, err?: string) => void) {
+		var params = {
+			action: 'GetCentury',
+			centuryID: centuryID
+		};
+
+		super.doPost(params, (res: any) => {
+			if (res.err) {
+				SpgProxy.callbackError(callback, res.err);
+			}
+			else {
+				callback(new cty.Century(res as cty.Properties), null);
+			}
+		});
+	}
+
+	getCenturies(callback: (centuries: Array<cty.Century>, err?: string) => void) {
+		var params = {
+			action: 'GetCenturies'
+		};
+
+		super.doPost(params, (res: any) => {
+			if (res.error) {
+				SpgProxy.callbackError(callback, res.err);
+			}
+			else if (res.constructor === Array) {
+				var centuries: Array<cty.Century> = [];
+				for (let i = 0; i < res.length; i++) {
+					centuries.push(new cty.Century(res[i] as cty.Properties));
+				}
+				callback(centuries, null)
+			}
+			else {
+				SpgProxy.callbackError(callback, null);
+			}
+		});
+	}
+
+	createCursus(props: crs.Properties, callback: (c: crs.Cursus, err?: string) => void) {
+		var params = {
+			action: 'CreateCursus',
+			cursus: props
+		};
+
+		super.doPost(params, (res: any) => {
+			if (res.err) {
+				SpgProxy.callbackError(callback, res.err);
+			}
+			else {
+				callback(new crs.Cursus(res as crs.Properties), null);
+			}
+		});
+	}
+
+	getCursus(cursusID: string, callback: (c: crs.Cursus, err?: string) => void) {
+		var params = {
+			action: 'GetCursus',
+			cursusID: cursusID
+		};
+
+		super.doPost(params, (res: any) => {
+			if (res.err) {
+				SpgProxy.callbackError(callback, res.err);
+			}
+			else {
+				callback(new crs.Cursus(res as crs.Properties), null);
+			}
+		});
+	}
+
+	getCursuses(callback: (arr: Array<crs.Cursus>, err?: string) => void) {
+		var params = {
+			action: 'GetCursuses',
+		};
+
+		super.doPost(params, (res: any) => {
+			if (res.err) {
+				SpgProxy.callbackError(callback, res.err);
+			}
+			else if (res.constructor === Array) {
+				var arr: Array<crs.Cursus> = [];
+				for (let i = 0; i < res.length; i++) {
+					arr.push(new crs.Cursus(res[i] as crs.Properties), null);
+				}
+				callback(arr, null);
+			}
+		});
 	}
 
 	/**
